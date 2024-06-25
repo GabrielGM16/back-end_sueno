e4eec999e1985eea85171ec0534758c919bc2dc8
GabrielGM16 <gmoficial16@gmail.com>
2024-06-25 16:35:15 -0600

    Base Datos EC2

--- config/db.js
+++ config/db.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    port: 3306,
-  user: 'root',
-  password: 'linux',
+  host: '54.210.84.99',
+  port: 3306,
+  user: 'gabo',
+  password: 'pato123',
   database: 'habit_tracker'
 });
 
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: "54.210.84.99",
+  host: '54.210.84.99',
   port: 3306,
   user: 'gabo',
   password: 'pato123',
