396ada785d8f65aeefea0d07f2d80089d9dc36b3
GabrielGM16 <gmoficial16@gmail.com>
2024-06-26 00:18:17 -0600

    amplify

--- controllers/authController.js
+++ controllers/authController.js
@@ -157,7 +157,7 @@ exports.forgotPassword = async (req, res) => {
       subject: 'Password Reset',
       text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n` +
         `Please click on the following link, or paste this into your browser to complete the process:\n\n` +
-        `https://mysueno-lymj.onrender.com/reset/${token}\n\n` +
+        `https://main.d3nk5qmj6p2cr8.amplifyapp.com/reset/${token}\n\n` +
         `If you did not request this, please ignore this email and your password will remain unchanged.\n`
     };
 
