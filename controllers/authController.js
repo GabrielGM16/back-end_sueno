6cb383aedd5238bf8bba451af41584aaaf95b062
GabrielGM16 <gmoficial16@gmail.com>
2024-06-26 23:20:54 -0600

    Reset pasword final

--- controllers/authController.js
+++ controllers/authController.js
@@ -224,7 +224,7 @@ exports.forgotPassword = async (req, res) => {
       subject: 'Password Reset',
       text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
              Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\n
-             https://main.d3nk5qmj6p2cr8.amplifyapp.com/reset-password/${resetPasswordToken}\n\n
+             https://main.d3nk5qmj6p2cr8.amplifyapp.com/reset/${resetPasswordToken}\n\n
              If you did not request this, please ignore this email and your password will remain unchanged.\n`
     };
 
